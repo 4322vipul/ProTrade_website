24c31605c4980fa86b695ce56b6ea5c82519dffc
Vipul Kumar Gupta <vipulkumargupta520@gmail.com>
2018-11-25 10:33:41 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,14 +1,33 @@
-from django.urls import path
-from .import views
+"""ProTrade URL Configuration
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-urlpatterns=[path("login",views.login_view,name="login"),
-             path("logout",views.logout_view,name="logout"),
-             path("home",views.home,name="home"),
-             path("buy",views.buy,name="buy"),
-             path("sell",views.sell,name="sell"),
-             path("swap",views.swap,name="swap"),
-             path("successPage",views.successPage,name="successPage"),
-             path("feedbackSuccessPage",views.feedbackSuccessPage,name="feedbackSuccessPage"),
-             path("contacts",views.contacts,name="contacts"),]
-             
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include('protrade.urls')),]
+'''
+if settings.DEBUG:
+    urlpatterns +=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+    urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)    
+    
+'''
+
+urlpatterns+=staticfiles_urlpatterns()
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
